77f541a48b6f92d9c308f0995a8ab711c54982fa
Chris <chrisldiep@gmail.com>
2020-10-05 12:27:00 -0700

    psuedo code completed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+// I was here -lillian
+
+var getElementsByClassName = function(className) {
   // your code here
+  var result = [];
+  //Call checkClass with document.body as input
+  return checkClass(document.body, className, result);
+};
+
+//Helper Function to check if the inputted element has a specific class
+var checkClass = function(element, className, result) {
+  //Check if element contains given class name & it has a class
+  if (element.classList && element.classList.contains(className)) {
+    //If true, push into result array
+    result.push(element);
+  //Check for and iterate through children elements
+  } else if (element.hasChildNodes()) {
+    for (let i = 0; i < element.childNodes.length; i++) {
+      //Call upon helper function checkClass with children elements as input
+      checkClass(element.childNodes[i]);
+    }
+  }
+  return result;
 };
