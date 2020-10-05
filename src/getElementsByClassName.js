0db8ab8622535220f72b564f861f6a329e2ea9a0
Chris <chrisldiep@gmail.com>
2020-10-05 15:18:48 -0700

    Completed getElementsByClassName.js with Helper Function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -8,40 +8,21 @@
 
 var getElementsByClassName = function(className) {
   // your code here
-  var result = [];
   //Call checkClass with document.body as input
-
-  //Helper Function to check if the inputted element has a specific class
-  var checkClass = function(element) {
+  checkClass(document.body, className, result = []);
+  return result;
+};
+//Helper Function to check if the inputted element has a specific class
+var checkClass = function(element, className, result) {
   //Check if element contains given class name & it has a class
-    if (element.classList && element.classList.contains(className)) {
-      //If true, push into result array
-      result.push(element);
-    //Check for and iterate through children elements
-    } if (element.hasChildNodes()) {
-      for (let index = 0; index < element.childNodes.length; index++) {
-        //Call upon helper function checkClass with children elements as input
-        checkClass(element.childNodes[index]);
-      }
+  if (element.classList && element.classList.contains(className)) {
+    //If true, push into result array
+    result.push(element);
+  //Check for and iterate through children elements
+  } if (element.hasChildNodes()) {
+    for (let index = 0; index < element.childNodes.length; index++) {
+      //Call upon helper function checkClass with children elements as input
+      checkClass(element.childNodes[index], className, result);
     }
-
+  }
 };
-checkClass(document.body);
-return result;
-};
-
-// //Helper Function to check if the inputted element has a specific class
-// var checkClass = function(element, className, result) {
-//   //Check if element contains given class name & it has a class
-//   if (element.classList && element.classList.contains(className)) {
-//     //If true, push into result array
-//     result.push(element);
-//   //Check for and iterate through children elements
-//   } else if (element.hasChildNodes()) {
-//     for (let i = 0; i < element.childNodes.length; i++) {
-//       //Call upon helper function checkClass with children elements as input
-//       checkClass(element.childNodes[i]);
-//     }
-//   }
-//   return result;
-// };
